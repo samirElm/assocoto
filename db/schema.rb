5877419e837595ed8d58c591ea1a6b4d44128acd
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2017-02-15 10:17:10 +0100

    Add attributes to User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215090717) do
+ActiveRecord::Schema.define(version: 20170215091503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20170215090717) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name",             default: ""
+    t.string   "last_name",              default: ""
+    t.string   "mobile_phone",           default: ""
+    t.string   "address",                default: ""
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
